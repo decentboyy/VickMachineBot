f6f67cb1ef68d1569ec730700a4347a6cc934c64
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-30 17:29:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ MONGO_URL = os.environ.get("mongodb+srv://abc:abcd@cluster0.dzijde4.mongodb.net/
 
 
 bot = Client(
-    "VickBot"
-    api_id = 18654447
-    api_hash = 60ac6f65c766e73dfcc1debef93d06bc
-    bot_token = 5058249365:AAGpAKZV8yV0aBXCaVkVOCEtWRIJQnv7V40
+    "VickBot",
+    api_id = 18654447,
+    api_hash = 60ac6f65c766e73dfcc1debef93d06bc,
+    bot_token = 5058249365:AAGpAKZV8yV0aBXCaVkVOCEtWRIJQnv7V40,
 )
 
 
